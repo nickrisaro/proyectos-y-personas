4309efd3ea975ec14b9454a009d9ca9ea98b37bf
Santiago Risaro <santiago.risaro@grupoesfera.com.ar>
2019-10-14 16:40:33 -0300

    Introduce el concepto de empresa

---
+++ empresa/empresa.go
@@ -0,0 +1,37 @@
+package empresa
+
+import (
+	"github.com/nickrisaro/proyectos-y-personas/persona"
+	"github.com/nickrisaro/proyectos-y-personas/proyecto"
+)
+
+// Empresa es la representación de una empresa, tiene empleados y proyectos
+type Empresa struct {
+	empleados []*persona.Persona
+	proyectos []*proyecto.Proyecto
+}
+
+// New construye una nueva empresa
+func New() *Empresa {
+	return new(Empresa)
+}
+
+// DarDeAltaEmpleado registra a la persona como empleada de la empresa
+func (e *Empresa) DarDeAltaEmpleado(persona *persona.Persona) {
+	e.empleados = append(e.empleados, persona)
+}
+
+// DarDeAltaProyecto registra el proyecto en la empresa
+func (e *Empresa) DarDeAltaProyecto(proyecto *proyecto.Proyecto) {
+	e.proyectos = append(e.proyectos, proyecto)
+}
+
+// Empleados retorna todos los empleados de la empresa
+func (e *Empresa) Empleados() []*persona.Persona {
+	return e.empleados
+}
+
+// Proyectos retorna todos los proyectos de la empresa
+func (e *Empresa) Proyectos() []*proyecto.Proyecto {
+	return e.proyectos
+}
---
+++ empresa/empresa_test.go
@@ -0,0 +1,38 @@
+package empresa_test
+
+import (
+	"testing"
+
+	"github.com/nickrisaro/proyectos-y-personas/empresa"
+	"github.com/nickrisaro/proyectos-y-personas/persona"
+	"github.com/nickrisaro/proyectos-y-personas/proyecto"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUnaEmpresaTieneEmpleados(t *testing.T) {
+
+	ana := persona.New("Ana", 1.0, persona.Senior, persona.Desarrollo, persona.Negociacion)
+	juan := persona.New("Juan", 1.0, persona.Junior, persona.Operaciones, persona.Investigacion)
+	empresa := empresa.New()
+
+	empresa.DarDeAltaEmpleado(ana)
+	empresa.DarDeAltaEmpleado(juan)
+
+	personas := []*persona.Persona{ana, juan}
+	assert.Equal(t, personas, empresa.Empleados(), "Los empleados de la empresa no son correctos")
+}
+
+func TestUnaEmpresaTieneProyectos(t *testing.T) {
+
+	personasRequeridas := proyecto.NewPersonasRequeridasPorSkill()
+	personasRequeridas.Desarrollo(1)
+	unProyecto := proyecto.New("Proyecto uno", personasRequeridas, 1.0)
+	otroProyecto := proyecto.New("Proyecto dos", personasRequeridas, 1.0)
+	empresa := empresa.New()
+
+	empresa.DarDeAltaProyecto(unProyecto)
+	empresa.DarDeAltaProyecto(otroProyecto)
+
+	proyectos := []*proyecto.Proyecto{unProyecto, otroProyecto}
+	assert.Equal(t, proyectos, empresa.Proyectos(), "Los proyectos de la empresa no son correctos")
+}
